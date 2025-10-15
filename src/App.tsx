0f37f9a4f439b532a54f82bbc9a7b37f34623863
Agaba Ronald <mischiefagaba@gmail.com>
2025-10-15 03:03:59 +0300

    ana

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { AuthProvider, useAuth } from './components/Auth/AuthProvider';
 import LoginForm from './components/Auth/LoginForm';
 import ModernAdminDashboard from './components/Admin/ModernAdminDashboard';
@@ -31,43 +33,44 @@ const AppContent = () => {
   }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/properties" element={<PropertyListings />} />
-          <Route path="/properties/:id" element={<PropertyDetailsPage />} />
-          <Route path="/sell-property" element={<PropertySellerForm />} />
-          <Route path="/submit-property" element={<PropertySellerForm />} />
-          <Route path="/buyer-request" element={<BuyerRequestForm />} />
-          <Route path="/book-visit" element={<SiteVisitForm />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/debug" element={<DebugPage />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute allowedRoles={['admin', 'super_admin']}>
-                <ModernAdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/properties" element={<PropertyListings />} />
+            <Route path="/properties/:id" element={<PropertyDetailsPage />} />
+            <Route path="/sell" element={<PropertySellerForm />} />
+            <Route path="/buy" element={<BuyerRequestForm />} />
+            <Route path="/book-visit" element={<SiteVisitForm />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/debug" element={<DebugPage />} />
+            <Route
+              path="/admin/*"
+              element={
+                <ProtectedRoute allowedRoles={['admin', 'super_admin']}>
+                  <ModernAdminDashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Analytics />
+          <SpeedInsights />
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <AppContent />
-      </Router>
+      <AppContent />
     </AuthProvider>
   );
 }
